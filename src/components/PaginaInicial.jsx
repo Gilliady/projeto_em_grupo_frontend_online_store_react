6e62df4ea40911d5a87091371f09cc93d174706e
Guilherme P Silva <guilpas@gmail.com>
2023-04-19 17:03:16 -0300

    Requisito 4

--- src/components/PaginaInicial.jsx
+++ src/components/PaginaInicial.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class PaginaInicial extends Component {
   state = {
     termoBusca: '',
+    categoriesList: [],
+  };
+
+  componentDidMount() {
+    this.getCategoriesBtn();
+  }
+
+  getCategoriesBtn = async () => {
+    const categoriesList = await getCategories();
+    this.setState({ categoriesList });
   };
 
   handleChange = (event) => {
@@ -14,7 +25,16 @@ export default class PaginaInicial extends Component {
   };
 
   render() {
-    const { termoBusca } = this.state;
+    const { termoBusca, categoriesList } = this.state;
+
+    const listaCategorias = categoriesList.map(({ id, name }) => (
+      <li key={ id }>
+        <label data-testid="category">
+          <input type="radio" name="" value="" />
+          {name}
+        </label>
+      </li>
+    ));
     return (
       <div>
         <label htmlFor="">
@@ -26,13 +46,16 @@ export default class PaginaInicial extends Component {
             onChange={ this.handleChange }
           />
         </label>
-        {
-          termoBusca === '' ? (
-            <p data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </p>)
-            : <ul />
-        }
+        {termoBusca === '' ? (
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        ) : (
+          <ul />
+        )}
+        <ul>
+          { listaCategorias }
+        </ul>
       </div>
     );
   }
