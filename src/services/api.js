4ba11815a356ae80987643d94aa83a7ac2c24f69
Gilliady <gilliadyazevedo@gmail.com>
2023-04-19 16:13:44 -0300

    finalizado requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
-  console.log('Hello the World');
+  const endpointCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(endpointCategories);
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const responseByCategoryIdAndQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  return responseByCategoryIdAndQuery.json();
 }
 
 export async function getProductById() {
